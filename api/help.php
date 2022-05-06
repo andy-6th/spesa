d490b1b93711befa3a6dbe199b744cb77b882836
root <root@vmi234334.contaboserver.net>
2022-05-07 01:22:48 +0200

    Update api/help

--- api/help.php
+++ api/help.php
@@ -11,23 +11,15 @@ API USAGE
 
 /api/get                    => list all items in cart and in backup list
 
-/api/add?name="item name"   => push item to main shopping list
+/api/add?name="item name"   => either push item to main shopping list, or
+                               resume item from backup list to main list
 
 /api/buy?name="item name"   => move item from main shopping list to backup list
 
-/api/res?name="item name"   => move item from backup list to main shopping list
-
 /api/del?name="item name"   => permanently remove item from backup list
 
-     In all the above endpoints, the parameter "item name" is case insensitive.
-
-/api/set?jsonstring=...     => set entire cart list and backup list
-
-     jsonstring is a json formatted array like 
-            [{"name":"item1","listed":"booleanValue"},...]
-     where each element has a non empty name and a "listed" property set to 
-     true to put the item in the cart or false to put it in the backup list.
-     Note that some characters like ":" might require a special html format.
+     In all the above endpoints, the parameter "item name" is case insensitive
+     and shoud be url-encoded.
 
 /*********************************************************************************/
 </pre>
